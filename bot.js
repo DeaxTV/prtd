5fadeb9e4be9dac92ef338488121c15b1c47bf3d
Glitch (hello-express) <none>
2020-10-10 13:47:08 +0000

    💾😚 Checkpoint ./README.md:21227326/2217 ./package.json:21227326/1293 ./bot.js:21227326/3143 ./ayarlar.json:21227326/24

---
+++ bot.js
@@ -0,0 +1,104 @@
+const express = require("express");//port açma içn
+const app = express();
+const http = require("http");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+const db = require("quick.db");
+const moment = require("moment");
+const ayarlar = require("./ayarlar.json");
+require("./yukle/eventci.js")(client);
+
+const log = message => {
+  console.log(`${moment().format("HH:mm:ss")} ${message}`);
+};
+
+//port açma :)
+app.get("/", (request, response) => {
+  console.log(`Request Yedim lan`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+client.commands = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  console.log(`${files.length} Komuta Sahibim`);
+  files.forEach(f => {
+    let kombo = require(`./komutlar/${f}`);
+    log(` ${kombo.help.name} ✅`);
+    client.commands.set(kombo.help.name, kombo);
+    kombo.conf.aliases.forEach(aliases => {
+      client.aliases.set(aliases, kombo.help.name);
+    });
+  });//conf
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.owner) permlvl = 4;
+  return permlvl;
+};
+
+
+client.login(ayarlar.token);
