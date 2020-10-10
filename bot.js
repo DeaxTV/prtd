93607c8b6f43a2f68bb165acb7881bcdfb73167d
Glitch (hello-express) <none>
2020-10-10 14:34:02 +0000

    👽🗻 Checkpoint ./bot.js:21227326/6888 ./package.json:21227326/1598 ./util/eventloader.js:21227326/191 ./util/Util.js:21227326/3662

--- bot.js
+++ bot.js
@@ -1,21 +1,26 @@
-const express = require("express");//port açma içn
-const app = express();
-const http = require("http");
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const chalk = require("chalk");
+const moment = require("moment");
+var Jimp = require("jimp");
+const { Client, Util } = require("discord.js");
+const weather = require("weather-js");
 const fs = require("fs");
 const db = require("quick.db");
-const moment = require("moment");
-const ayarlar = require("./ayarlar.json");
-require("./yukle/eventci.js")(client);
+const http = require("http");
+const express = require("express");
+require("./util/eventLoader.js")(client);
+const path = require("path");
+const request = require("request");
+const snekfetch = require("snekfetch");
+const queue = new Map();
+const YouTube = require("simple-youtube-api");
+const ytdl = require("ytdl-core");
 
-const log = message => {
-  console.log(`${moment().format("HH:mm:ss")} ${message}`);
-};
-
-//port açma :)
+const app = express();
 app.get("/", (request, response) => {
-  console.log(`Request Yedim lan`);
+  console.log(Date.now() + "KEDI GOZU NEBULASI | KEDI GOZU BULUTUSU");
   response.sendStatus(200);
 });
 app.listen(process.env.PORT);
@@ -23,18 +28,25 @@ setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
 
+var prefix = ayarlar.prefix;
+
+const log = message => {
+  console.log(`${message}`);
+};
+
 client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 fs.readdir("./komutlar/", (err, files) => {
   if (err) console.error(err);
-  console.log(`${files.length} Komuta Sahibim`);
+  log(`${files.length} komut yüklenecek.`);
   files.forEach(f => {
-    let kombo = require(`./komutlar/${f}`);
-    log(` ${kombo.help.name} ✅`);
-    client.commands.set(kombo.help.name, kombo);
-    kombo.conf.aliases.forEach(aliases => {
-      client.aliases.set(aliases, kombo.help.name);
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
     });
-  });//conf
+  });
 });
 
 client.reload = command => {
@@ -88,7 +100,6 @@ client.unload = command => {
   });
 };
 
-
 client.elevation = message => {
   if (!message.guild) {
     return;
@@ -96,10 +107,22 @@ client.elevation = message => {
   let permlvl = 0;
   if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
   if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
-  if (message.author.id === ayarlar.owner) permlvl = 4;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
   return permlvl;
 };
 
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   l0RDconsole.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// }); //DEVİLHOUSE//
+
+client.on("warn", e => {
+  console.log(chalk.bgYellow(e.replace(regToken, "that was redacted")));
+});
+
+client.on("error", e => {
+  console.log(chalk.bgRed(e.replace(regToken, "that was redacted")));
+});
 
 client.login(ayarlar.token);
 //sa-as
