9c4254e3f47f24487b84dda8c14fd9eb9179fb8c
Glitch (hello-express) <none>
2022-01-29 10:20:00 +0000

    🐁👙 Checkpoint ./bot.js:41571202/7016

--- bot.js
+++ bot.js
@@ -1,17 +1,17 @@
-const Discord = require("discord.js");//
-const client = new Discord.Client();//
-const ayarlar = require("./ayarlar.json");//
-const chalk = require("chalk");//
-const moment = require("moment");//
-var Jimp = require("jimp");//
-const { Client, Util } = require("discord.js");//
-const weather = require("weather-js");//
-const fs = require("fs");//
+const Discord = require("discord.js"); //
+const client = new Discord.Client(); //
+const ayarlar = require("./ayarlar.json"); //
+const chalk = require("chalk"); //
+const moment = require("moment"); //
+var Jimp = require("jimp"); //
+const { Client, Util } = require("discord.js"); //
+const weather = require("weather-js"); //
+const fs = require("fs"); //
 const db = require("quick.db");
 const http = require("http");
-const express = require("express");//
-require("./util/eventLoader.js")(client);//
-const path = require("path");//
+const express = require("express"); //
+require("./util/eventLoader.js")(client); //
+const path = require("path"); //
 const request = require("request");
 const snekfetch = require("snekfetch");
 const queue = new Map();
@@ -24,58 +24,59 @@ app.get("/", (request, response) => {
   response.sendStatus(200);
 });
 app.listen(process.env.PORT);
-setInterval(() => {//emir321r
+setInterval(() => {
+  //emir321r
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
 
 var prefix = ayarlar.prefix;
 
-const log = message => {
+const log = (message) => {
   console.log(`${message}`);
 };
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 fs.readdir("./komutlar/", (err, files) => {
-  if (err) console.error(err);//
+  if (err) console.error(err); //
   log(`${files.length} komut yüklenecek.`);
-  files.forEach(f => {
+  files.forEach((f) => {
     let props = require(`./komutlar/${f}`);
     log(`Yüklenen komut: ${props.help.name}.`);
     client.commands.set(props.help.name, props);
-    props.conf.aliases.forEach(alias => {
+    props.conf.aliases.forEach((alias) => {
       client.aliases.set(alias, props.help.name);
     });
   });
 });
 
-
-
-client.reload = command => {
+client.reload = (command) => {
   return new Promise((resolve, reject) => {
     try {
       delete require.cache[require.resolve(`./komutlar/${command}`)];
       let cmd = require(`./komutlar/${command}`);
       client.commands.delete(command);
-      client.aliases.forEach((cmd, alias) => {//
+      client.aliases.forEach((cmd, alias) => {
+        //
         if (cmd === command) client.aliases.delete(alias);
       });
       client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
+      cmd.conf.aliases.forEach((alias) => {
         client.aliases.set(alias, cmd.help.name);
       });
       resolve();
-    } catch (e) {//
+    } catch (e) {
+      //
       reject(e);
     }
   });
 };
 
-client.load = command => {
+client.load = (command) => {
   return new Promise((resolve, reject) => {
     try {
       let cmd = require(`./komutlar/${command}`);
       client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
+      cmd.conf.aliases.forEach((alias) => {
         client.aliases.set(alias, cmd.help.name);
       });
       resolve();
@@ -85,7 +86,7 @@ client.load = command => {
   });
 };
 
-client.unload = command => {
+client.unload = (command) => {
   return new Promise((resolve, reject) => {
     try {
       delete require.cache[require.resolve(`./komutlar/${command}`)];
@@ -101,7 +102,7 @@ client.unload = command => {
   });
 };
 
-client.elevation = message => {
+client.elevation = (message) => {
   if (!message.guild) {
     return;
   }
@@ -110,16 +111,15 @@ client.elevation = message => {
   if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
   if (message.author.id === ayarlar.sahip) permlvl = 4;
   return permlvl;
-};//
+}; //
 
 var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
 
-
-client.on("warn", e => {
+client.on("warn", (e) => {
   console.log(chalk.bgYellow(e.replace(regToken, "that was redacted")));
 });
 
-client.on("error", e => {
+client.on("error", (e) => {
   console.log(chalk.bgRed(e.replace(regToken, "that was redacted")));
 });
 
