fad3e8d679151be98372150f699e16c8088af343
Glitch (luxurious-robust-bolt) <none>
2022-10-28 10:50:26 +0000

    📊💨 Checkpoint ./src/components/Cursor.js:40920016/12 ./public/styles/globals.css:40920016/476 ./src/components/Cursor.jsx:40920016/529

---
+++ src/components/Cursor.jsx
@@ -0,0 +1,19 @@
+import getImage from 'util/getImage';
+const Cursor = () => {
+const dot = useRef(null);
+const dotOutline = useRef(null);
+const list = useRef(null) ;
+return(
+  <>
+<div ref={dotOutline} className="cursor-dot-outline"></div>
+<div ref={dot} className="cursor-dot"></div>
+       
+<div ref={list} className="links-container">
+{[...Array(4).keys()].map(i =>(
+<Link key={i} src={getImage(i + 1)} />
+                          ))}
+</div>
+</>
+                  );
+                  };
+export default Cursor;
